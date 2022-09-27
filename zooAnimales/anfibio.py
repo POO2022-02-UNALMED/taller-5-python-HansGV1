2bbd44e08efe08182a9169f824b3f2fc632e2761
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-09-27 14:19:59 -0500

    update

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -1,6 +1,6 @@
 from gestion import zoologico
 
-class Anfibio:
+class Anfibio(Animal):
     def __init__ (self, anfibio, ranas, salamandras, colorPiel, venenoso, numAnfi):
         self.anfibio = anfibio
         self.ranas = ranas
